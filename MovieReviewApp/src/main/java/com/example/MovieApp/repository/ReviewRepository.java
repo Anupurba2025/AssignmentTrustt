1cf6d0970e3547029dd86b8b90407e8a749f48ac
Anupurba Khan <anupurba2025@gmail.com>
2024-11-13 18:59:15 +0530

    Deleted MovieReviewApp folder

--- MovieReviewApp/src/main/java/com/example/MovieApp/repository/ReviewRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MovieApp.repository;
-
-import com.example.MovieApp.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
